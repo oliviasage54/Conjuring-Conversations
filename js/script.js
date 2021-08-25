8d7f9ac5ddbed3fd4ff00b5e823293e3e4fd4979
Olivia Adams <65257375+oliviasage54@users.noreply.github.com>
2021-08-25 09:25:23 -0400

    episodes page responsiveness
    i dont remember what i did but it was probably important

--- js/script.js
+++ js/script.js
@@ -1,15 +1,33 @@
+/*Script for the navigation link changing when you are on that page*/
+/*Home page link*/
+var homePageLink = document.getElementById("homeLink");
+var homeDiv = document.getElementById("h");
+/*Episodes page*/
+var episodesPageLink = document.getElementById("episodesLink");
+var episodesDiv = document.getElementById("e");
+/*submissions page*/
+var submissionsPageLink = document.getElementById("subLink");
+var submissionsDiv = document.getElementById("su");
+/*Meet the team page*/ 
+var meetTeamPageLink = document.getElementById("teamLink");
+var meetDiv = document.getElementById("m");
+/*Shop page*/
+var shopPageLink = document.getElementById("shopLink");
+var shopDiv = document.getElementById("sh");
 
- /*JQuery for responsive mobile nav
- rewatch this video for full explanation
-https://www.youtube.com/watch?v=97njQRCVxPU */
 
-jQuery(document).ready(function(){
-
-    jQuery(".menuButton").click(function(){
-        jQuery(".navDiv").slideToggle(400, function(){
-            jQuery(this).toggleClass("navExpanded").css('display','')
-        });
-    });
-});
-
-
+if(document.URL.includes("index.html")){
+    
+}
+else if(document.URL.includes("episodes.html")){
+    
+}
+else if(document.URL.includes("meetTheTeam.html")){
+    
+}
+else if(document.URL.includes("shop.html")){
+    
+}
+else if(document.URL.includes("submissions.html")){
+    
+}
